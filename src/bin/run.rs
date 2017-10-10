0d58dd3495b3d1cf3fdd298d49d476becf4d359a
David Pedersen <david.pdrsn@gmail.com>
2017-10-10 17:09:19 +0200

    Move log parsing code into its own file

--- src/bin/run.rs
+++ src/bin/run.rs
@@ -1,11 +1,11 @@
 extern crate instant_replay;
 
-use instant_replay::{Request};
+// use instant_replay::{Request};
 
 fn main() {
-    let reqs = Request::from_logs_file(&"test_fixtures/logs".to_string());
+    // let reqs = Request::from_logs_file(&"test_fixtures/logs".to_string());
 
-    for r in reqs {
-        println!("{:?}", r);
-    }
+    // for r in reqs {
+    //     println!("{:?}", r);
+    // }
 }
