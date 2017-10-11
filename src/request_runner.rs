bc7976c7fab1eccee57c46780af2a124baabe575
David Pedersen <david.pdrsn@gmail.com>
2017-10-11 17:48:44 +0200

    Remove binaries used while developing

--- src/request_runner.rs
+++ src/request_runner.rs
@@ -26,16 +26,7 @@ impl RequestRunner {
     pub fn run_request(&mut self, host: &String, request: &Request) -> Result<Response, Error> {
         let uri = format!("{}/{}", host, request.url).parse().unwrap();
         let mut http_req = hyper::Request::new(Method::Get, uri);
-
         let header = format!("Bearer {}", request.access_token.clone());
-
-        // println!(
-        //     "token: {}, slug: {}, url: {}",
-        //     request.access_token,
-        //     request.user_slug,
-        //     request.url,
-        //     );
-
         http_req.headers_mut().set(Authorization(header));
         let get = self.client.request(http_req);
         self.core.run(get)
