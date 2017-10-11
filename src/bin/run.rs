cef29583dbacba1edf2eb0fb0b0107bb6f2c21bf
David Pedersen <david.pdrsn@gmail.com>
2017-10-11 08:59:01 +0200

    Prepare for demo

--- src/bin/run.rs
+++ src/bin/run.rs
@@ -6,18 +6,32 @@ use instant_replay::{Request, RequestRunner};
 use instant_replay::time_iter::repeat_for;
 use std::io::{self, Write};
 use std::time::{Duration};
+use std::thread::{spawn, JoinHandle};
+use std::sync::Arc;
 
 fn main() {
     let reqs = Request::from_logs_file(&"test_fixtures/logs".to_string());
-    let req = reqs.first().unwrap();
+    let req = Arc::new(reqs.first().unwrap().clone());
 
-    let mut runner = RequestRunner::new();
+    let thread_count = 10;
 
-    for _ in repeat_for(Duration::from_secs(10)) {
-        match runner.run_request(&req) {
-            Ok(_) => print!("."),
-            Err(_) => print!("f"),
-        };
-        io::stdout().flush().expect("Flushing failed");
+    let threads = (1..(thread_count + 1)).map(|_| {
+        let req = Arc::clone(&req);
+
+        spawn(move || {
+            let mut runner = RequestRunner::new();
+
+            for _ in repeat_for(Duration::from_secs(10)) {
+                match runner.run_request(&req) {
+                    Ok(_) => print!("."),
+                    Err(_) => print!("f"),
+                };
+                io::stdout().flush().expect("Flushing failed");
+            }
+        })
+    }).collect::<Vec<JoinHandle<()>>>();
+
+    for t in threads {
+        t.join().unwrap();
     }
 }
